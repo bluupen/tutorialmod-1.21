2d0cea64b252d8d68d0b06d0dbac876026e43be0
bluupen <rain061942@gmail.com>
2025-08-07 20:59:38 +0900

    #3 custom blocks

--- src/main/java/bluupen/tutorialmod/TutorialMod.java
+++ src/main/java/bluupen/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package bluupen.tutorialmod;
 
+import bluupen.tutorialmod.block.ModBlocks;
 import bluupen.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
