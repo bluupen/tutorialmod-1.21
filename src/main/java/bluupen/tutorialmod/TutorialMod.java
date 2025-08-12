7b31d63999547b9d8955e56f64e45c638139a07b
bluupen <rain061942@gmail.com>
2025-08-12 16:13:34 +0900

    #7 custom advanced blocks

--- src/main/java/bluupen/tutorialmod/TutorialMod.java
+++ src/main/java/bluupen/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package bluupen.tutorialmod;
 
 import bluupen.tutorialmod.block.ModBlocks;
+import bluupen.tutorialmod.item.ModItemGroups;
 import bluupen.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
--- src/main/java/bluupen/tutorialmod/block/ModBlocks.java
+++ src/main/java/bluupen/tutorialmod/block/ModBlocks.java
@@ -1,9 +1,12 @@
 package bluupen.tutorialmod.block;
 
 import bluupen.tutorialmod.TutorialMod;
+import bluupen.tutorialmod.block.Custom.MagicBlock;
+
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,16 +14,25 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
-
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
             new Block(AbstractBlock.Settings.create()
                     .strength(4f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
-
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
             new Block(AbstractBlock.Settings.create()
                     .strength(3f).requiresTool()));
+
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+        new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                AbstractBlock.Settings.create().strength(3f).requiresTool()));
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+    public static final Block MAGIC_BLOCK = registerBlock("magic_block",
+            new MagicBlock(AbstractBlock.Settings.create().strength(1f).requiresTool()));
 
 
     private static Block registerBlock(String name, Block block) {
